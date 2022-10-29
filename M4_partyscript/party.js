8027c7d47654923e09e92602c8b20340fb9bddbf
nouratieh <99181948+nouratieh@users.noreply.github.com>
2022-10-29 13:57:06 +0300

    Update party.js

--- M4_partyscript/party.js
+++ M4_partyscript/party.js
@@ -11,13 +11,17 @@ do {
     if (age >=minAge   && age <= maxAge ) {
     let person=prompt("What is your Name");
     //let welcome= "Hello " + person + "! How are you today?";
-    console.log("Hello " + person + "! How are you today?")}
+    alert("Hello " + person + "! How are you today?")}
 
-    else {    
-      console.log("you are too old")
+    else if (age > maxAge ) {    
+     alert("you are too old")
+  }
+  else{
+    alert("you are too young")
   }
   }
   // if current person is younger than 18, will print (you are too young)
   // if older than 35, will print (you are too old)
   // if between 18 and 35, ask for the user name, and welcome the user by his/her name.
  while (confirm("Is there anyone else?"));
+
